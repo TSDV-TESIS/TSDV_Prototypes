c5320759fb4230950cd7d4dea945769f6bcacfc0
Nicodragon25 <nicolasariellaure@gmail.com>
2025-03-30 21:13:05 -0300

    Add speed based hook

--- Assets/Scripts/Player/Hook.cs
+++ Assets/Scripts/Player/Hook.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections;
 using Player;
 using UnityEngine;
 
@@ -7,11 +7,17 @@ public class Hook : MonoBehaviour
 {
     [SerializeField] private InputHandler handler;
     [SerializeField] private float coolDown;
+
+    [Header("Ray Settings")]
     [SerializeField] private float maxDistance;
     [SerializeField] private LayerMask rayLayerMask;
 
+    [Header("Displacement")]
+    [SerializeField] private float speed;
+
     private float lastHookTime = 0;
     private CharacterController _characterController;
+    private Coroutine _displacement;
 
     private void OnEnable()
     {
@@ -40,12 +46,32 @@ public class Hook : MonoBehaviour
         lastHookTime = Time.time;
         if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, maxDistance, rayLayerMask))
         {
-            _characterController.Move(hit.point - transform.position);
+            if (_displacement != null)
+                StopCoroutine(_displacement);
+            _displacement = StartCoroutine(MoveTowards(hit.point));
         }
     }
 
     private bool CanHook()
     {
         return Time.time - lastHookTime > coolDown;
+    }
+
+    private IEnumerator MoveTowards(Vector3 endPosition)
+    {
+        Vector3 initialPosition = transform.position;
+
+        float travelDuration = Vector3.Distance(initialPosition, endPosition) / speed;
+
+        float timer = 0;
+        float startTime = Time.time;
+
+        while (timer < travelDuration)
+        {
+            Vector3 movement = Vector3.Lerp(initialPosition, endPosition, timer / travelDuration);
+            _characterController.Move(movement - transform.position);
+            timer = Time.time - startTime;
+            yield return null;
+        }
     }
 }
