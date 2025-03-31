357cd067691befc8d025286cf8d898b33f6f83a9
Nicodragon25 <nicolasariellaure@gmail.com>
2025-03-30 22:13:20 -0300

    Add hook visuals

--- Assets/Scripts/Player/Hook.cs
+++ Assets/Scripts/Player/Hook.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using Player;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -13,13 +14,19 @@ public class Hook : MonoBehaviour
     [SerializeField] private float maxDistance;
     [SerializeField] private LayerMask rayLayerMask;
 
+    [FormerlySerializedAs("speed")]
     [Header("Displacement")]
-    [SerializeField] private float speed;
+    [SerializeField] private float moveSpeed;
     [SerializeField] private AnimationCurve accelerationCurve;
+
+    [Header("Visuals")]
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private float throwSpeed;
 
     private float lastHookTime = 0;
     private CharacterController _characterController;
     private Coroutine _displacement;
+    private Coroutine _hookVisual;
 
     private void OnEnable()
     {
@@ -52,6 +59,12 @@ public class Hook : MonoBehaviour
                 StopCoroutine(_displacement);
             _displacement = StartCoroutine(MoveTowards(hit.point));
         }
+        else
+        {
+            if (_hookVisual != null)
+                StopCoroutine(_hookVisual);
+            _hookVisual = StartCoroutine(ShootHook(transform.forward * maxDistance, true));
+        }
     }
 
     private bool CanHook()
@@ -59,21 +72,46 @@ public class Hook : MonoBehaviour
         return Time.time - lastHookTime > coolDown;
     }
 
+    private IEnumerator ShootHook(Vector3 position, bool shouldDecay)
+    {
+        lineRenderer.enabled = true;
+        Vector3 initialPos = transform.position;
+        float duration = Vector3.Distance(initialPos, position) / throwSpeed;
+        float timer = 0;
+        float startTime = Time.time;
+        while (timer < duration)
+        {
+            lineRenderer.SetPosition(0, transform.position);
+            timer = Time.time - startTime;
+            Vector3 hookPosition = Vector3.Lerp(initialPos, position, timer / duration);
+            lineRenderer.SetPosition(1, hookPosition);
+            yield return null;
+        }
+
+        if (shouldDecay)
+            lineRenderer.enabled = false;
+    }
+
     private IEnumerator MoveTowards(Vector3 endPosition)
     {
+        yield return ShootHook(endPosition, false);
         Vector3 initialPosition = transform.position;
 
-        float travelDuration = Vector3.Distance(initialPosition, endPosition) / speed;
+        float travelDuration = Vector3.Distance(initialPosition, endPosition) / moveSpeed;
 
         float timer = 0;
         float startTime = Time.time;
 
         while (timer < travelDuration)
         {
+            lineRenderer.SetPosition(0, transform.position);
+
             Vector3 movement = Vector3.Lerp(initialPosition, endPosition, accelerationCurve.Evaluate(timer / travelDuration));
             _characterController.Move(movement - transform.position);
             timer = Time.time - startTime;
             yield return null;
         }
+
+        lineRenderer.enabled = false;
     }
 }
