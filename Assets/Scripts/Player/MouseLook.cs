5506a708413ff8ee4091add60af2f4e4fb66b89d
Nicodragon25 <nicolasariellaure@gmail.com>
2025-04-06 17:57:54 -0300

    Add 2d movement

--- Assets/Scripts/Player/MouseLook.cs
+++ Assets/Scripts/Player/MouseLook.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     [SerializeField] private InputHandler handler;
+    [SerializeField] private bool is2D;
+    [SerializeField] private GameObject visorPivot;
 
     private float _angle;
 
@@ -24,6 +26,12 @@ public class MouseLook : MonoBehaviour
         viewPortPos -= new Vector2(0.5f, 0.5f);
 
         _angle = Mathf.Atan2(viewPortPos.x, viewPortPos.y) * Mathf.Rad2Deg;
+        if (is2D)
+        {
+            visorPivot.transform.rotation = Quaternion.AngleAxis(-_angle + 90, Vector3.forward) * transform.rotation;
+            return;
+        }
+
         transform.rotation = Quaternion.AngleAxis(_angle, Vector3.up);
     }
 }
---
+++ Assets/Scripts/Player/Movement2D.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class Movement2D : MonoBehaviour
+    {
+        [SerializeField] private float velocity;
+        [SerializeField] private InputHandler handler;
+
+        private CharacterController _characterController;
+        private Vector3 _moveDirection;
+        private bool _canWalk;
+
+        void OnEnable()
+        {
+            _canWalk = true;
+            _characterController ??= GetComponent<CharacterController>();
+            _moveDirection = Vector3.zero;
+
+            handler.OnPlayerMove.AddListener(HandleMove);
+        }
+
+        private void OnDisable()
+        {
+            handler.OnPlayerMove.RemoveListener(HandleMove);
+        }
+
+        void Update()
+        {
+            if (_canWalk)
+                _characterController.Move(_moveDirection * (Time.deltaTime * velocity));
+        }
+
+        private void HandleMove(Vector2 movement)
+        {
+            _moveDirection = new Vector3(movement.x, 0, 0);
+        }
+
+        public void SetCanWalk(bool canWalk)
+        {
+            _canWalk = canWalk;
+        }
+    }
+}
