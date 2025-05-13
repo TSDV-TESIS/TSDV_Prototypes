6035eaffda3166369aa6311606253f18739ad03f
Nicodragon25 <nicolasariellaure@gmail.com>
2025-05-13 14:57:40 -0300

    Add attackDisplacement vfx

--- Assets/Scripts/Player/Attacks/Attack.cs
+++ Assets/Scripts/Player/Attacks/Attack.cs
@@ -12,6 +12,7 @@ namespace Player
         [Header("Attack properties")]
         [SerializeField] private PlayerAttackProperties attackProperties;
 
+        private ShadowStep _shadows;
         private PlayerMovement _playerMovement;
         private MouseLook _mouseLook;
         private Coroutine _attackCoroutine;
@@ -21,6 +22,7 @@ namespace Player
         {
             _playerMovement ??= GetComponent<PlayerMovement>();
             _mouseLook ??= GetComponent<MouseLook>();
+            _shadows ??= GetComponent<ShadowStep>();
         }
 
         void OnEnable()
@@ -48,6 +50,8 @@ namespace Player
             attackObject.SetActive(true);
             float timer = 0;
             float startTime = Time.time;
+
+            _shadows.InitShadowStepShadows();
             while (timer < attackProperties.duration)
             {
                 _playerMovement.Velocity = _mouseLook.CursorDir * attackProperties.displacementForce;
@@ -55,6 +59,7 @@ namespace Player
                 yield return null;
             }
 
+            _shadows.StopShadows();
             attackObject.SetActive(false);
             yield return new WaitForSeconds(attackProperties.coolDownDuration);
             _isAttacking = false;
