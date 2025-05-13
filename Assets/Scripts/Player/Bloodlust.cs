cbd0a2a2b87e13d281c7280e4a2237b99739eb43
Maxi Feldman <maximiliano.a.feldman@gmail.com>
2025-05-13 17:48:44 -0300

    Add Frenzied state

--- Assets/Scripts/Player/Bloodlust.cs
+++ Assets/Scripts/Player/Bloodlust.cs
@@ -8,46 +8,67 @@ namespace Player
     public class Bloodlust : MonoBehaviour
     {
         [SerializeField] private BloodlustProperties bloodlustProperties;
-        
-        [Header("Handlers")]
-        [SerializeField] private InputHandler handler;
-    
-        [Header("Events")] 
-        [SerializeField] private VoidEventChannelSO onBloodlustStart;
+
+        [Header("Handlers")] [SerializeField] private InputHandler handler;
+
+        [Header("Events")] [SerializeField] private VoidEventChannelSO onBloodlustStart;
         [SerializeField] private VoidEventChannelSO onBloodlustEnd;
         [SerializeField] private FloatEventChannel onBloodlustUsage;
         [SerializeField] private VoidEventChannelSO onFrenzyStart;
+        [SerializeField] private VoidEventChannelSO onFrenzyEnd;
 
         private bool _isInBloodlust;
+        private bool _isFrenzied;
         private float _bloodlustUsagePercentageLeft;
         private float _bloodlustUsageSeconds;
+
         private void OnEnable()
         {
             _bloodlustUsageSeconds = 0;
             _bloodlustUsagePercentageLeft = 1;
             _isInBloodlust = false;
             handler.OnPlayerBloodlust.AddListener(HandleBloodlust);
+
+            onFrenzyEnd?.onEvent.AddListener(HandleFrenzyEnd);
+            onFrenzyStart?.onEvent.AddListener(HandleFrenzyStart);
         }
 
         private void OnDisable()
         {
             handler.OnPlayerBloodlust.RemoveListener(HandleBloodlust);
+            onFrenzyEnd?.onEvent.RemoveListener(HandleFrenzyEnd);
+            onFrenzyStart?.onEvent.RemoveListener(HandleFrenzyStart);
+        }
+
+        private void HandleFrenzyEnd()
+        {
+            _isFrenzied = false;
+        }
+        
+        private void HandleFrenzyStart()
+        {
+            Debug.Log("FRENZIEDDDDDD");
+            _isFrenzied = true;
+            onBloodlustEnd?.RaiseEvent();
+            _isInBloodlust = false;
         }
 
         void Update()
         {
-            if (_bloodlustUsagePercentageLeft == 1 && !_isInBloodlust ||
-                _bloodlustUsagePercentageLeft == 0 && _isInBloodlust) return;
+            //Debug.Log($"{_isFrenzied}, {(_bloodlustUsagePercentageLeft == 1 && !_isInBloodlust)}, {(_bloodlustUsagePercentageLeft == 0 && _isInBloodlust)}");
+            if (_isFrenzied || (_bloodlustUsagePercentageLeft == 1 && !_isInBloodlust) ||
+                (_bloodlustUsagePercentageLeft == 0 && _isInBloodlust)) return;
             if (_isInBloodlust)
             {
-                _bloodlustUsageSeconds += Time.deltaTime; 
+                _bloodlustUsageSeconds += Time.deltaTime;
             }
             else
             {
                 _bloodlustUsageSeconds -= Time.deltaTime * bloodlustProperties.bloodlustRecoveryVelocity;
             }
+
             _bloodlustUsagePercentageLeft = 1 - _bloodlustUsageSeconds / bloodlustProperties.bloodlustDuration;
-            
+
             if (_bloodlustUsagePercentageLeft > 1) _bloodlustUsagePercentageLeft = 1;
             if (_bloodlustUsagePercentageLeft < 0) _bloodlustUsagePercentageLeft = 0;
 
@@ -56,6 +77,7 @@ namespace Player
                 onBloodlustEnd?.RaiseEvent();
                 _isInBloodlust = false;
             }
+
             onBloodlustUsage?.RaiseEvent(_bloodlustUsagePercentageLeft);
         }
 
@@ -81,7 +103,7 @@ namespace Player
 
         private bool CanDoBloodlust()
         {
-            return _bloodlustUsagePercentageLeft > bloodlustProperties.minimumBloodlustPercentageUsage;
+            return !_isFrenzied && _bloodlustUsagePercentageLeft > bloodlustProperties.minimumBloodlustPercentageUsage;
         }
     }
 }
--- Assets/Scripts/Player/Frenzy.cs
+++ Assets/Scripts/Player/Frenzy.cs
@@ -1,16 +1,44 @@
+using System;
+using System.Collections;
+using Events;
 using UnityEngine;
 
-public class Frenzy : MonoBehaviour
+namespace Player
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class Frenzy : MonoBehaviour
     {
+        [SerializeField] private FrenzyProperties frenzyProperties;
         
-    }
+        [Header("Events")]
+        [SerializeField] private VoidEventChannelSO onFrenzyStart;
+        [SerializeField] private VoidEventChannelSO onFrenzyEnd;
 
-    // Update is called once per frame
-    void Update()
-    {
+        private Coroutine _frenzyCoroutine;
+        private void OnEnable()
+        {
+            onFrenzyStart?.onEvent.AddListener(HandleStartFrenzy);
+        }
+
+        private void OnDisable()
+        {
+            onFrenzyStart?.onEvent.RemoveListener(HandleStartFrenzy);
+        }
         
+        private void HandleStartFrenzy()
+        {
+            if(_frenzyCoroutine != null) StopCoroutine(_frenzyCoroutine);
+            _frenzyCoroutine = StartCoroutine(FrenziedCoroutine());
+        }
+
+        private IEnumerator FrenziedCoroutine()
+        {
+            float timer = 0;
+            while (timer < frenzyProperties.frenziedDuration)
+            {
+                timer += Time.deltaTime;
+                yield return null;
+            }
+            onFrenzyEnd?.RaiseEvent();
+        }
     }
 }
